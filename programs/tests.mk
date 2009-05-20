53aeebe01c7c0c19d6a89726d767a2d7f517842f
Raphael 'kena' Poss <r.c.poss@uva.nl>
2009-05-20 09:27:32 +0000

    [mgsim] update to use Automake 1.11: this allows make V=0 and other niceties!
    git-svn-id: https://svn.svp-home.org/sw/svp/mg/mgsim/trunk@2103 e97e5017-a994-416e-809d-76780e9f78db

--- programs/tests.mk
+++ programs/tests.mk
@@ -1,6 +1,7 @@
 EXTRA_DIST = $(TEST_SOURCES)
 
-TESTS_ENVIRONMENT = \
+
+LOG_COMPILER = \
    MGSIM="$(top_builddir)/src/mgsim-$(ARCH).dbg -c $(top_srcdir)/programs/config.ini -t" \
    $(SHELL) $(top_srcdir)/programs/runtest.sh $(ARCH) $(top_srcdir)/programs/timeout
 
