564c099d742d6c256e5fabbe00354aa71c656e0e
Raphael 'kena' Poss <r.c.poss@uva.nl>
2010-01-12 18:29:07 +0000

    [mgsim] add missing include to stdint.h
    git-svn-id: https://svn.svp-home.org/sw/svp/mg/mgsim/trunk@3350 e97e5017-a994-416e-809d-76780e9f78db

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@ const char* semaphore_journal = "/tmp/simx-sem-journal";
 #include <iostream>
 #include <iomanip>
 #include <limits>
+#include <stdint.h>
 #include <typeinfo>
 #include <cmath>
 #include <algorithm>
