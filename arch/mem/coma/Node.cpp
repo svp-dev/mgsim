90bf310630430446c91b3c3c84e51e37616cdd28
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-06-09 07:08:21 +0000

    [mgsim] Report the name of the next node in COMA network deadlock messages.
    git-svn-id: https://svn.svp-home.org/sw/svp/mg/mgsim/trunk@4533 e97e5017-a994-416e-809d-76780e9f78db

--- arch/mem/coma/Node.cpp
+++ arch/mem/coma/Node.cpp
@@ -130,7 +130,7 @@ Result COMA::Node::DoForward()
     
     if (!m_next->m_incoming.Push( m_outgoing.Front() ))
     {
-        DeadlockWrite("Unable to send request to next node");
+        DeadlockWrite("Unable to send request to next node (%s)", m_next->GetFQN().c_str());
         return FAILED;
     }
     m_outgoing.Pop();
