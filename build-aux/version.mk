9d387c9de5e35c780540faed40a8c6bd2d02c193
Raphael 'kena' Poss <r.c.poss@uva.nl>
2012-01-08 10:41:46 +0100

    Adjust the packaging after the migration to git.
    * build-aux/git-version-gen: New.
    
    * build-aux/version.mk: Import from sl-core. Disable `dist-hook'
    as it is provided by Makefile.am.
    
    * configure.ac,
    * Makefile.am: Use `version.mk' and `git-version-gen'.
    
    * Makefile.am (ChangeLog): Use `git log --stat'.
    
    * build-aux/users,
    * build-aux/gitlog2changelog.py,
    * build-aux/svn2log.py,
    * build-aux/qp_xml.py,
    * build-aux/version-gen: Remove.

---
+++ build-aux/version.mk
@@ -0,0 +1,31 @@
+##
+## Version number management
+##
+# (inspired from the same code in sl-core)
+
+EXTRA_DIST += .version build-aux/git-version-gen
+BUILT_SOURCES = $(top_srcdir)/.version
+
+$(top_srcdir)/.version:
+	echo $(VERSION) >$@-t && mv $@-t $@
+
+## dist-hook overridden in Makefile.am
+#dist-hook: check-version
+#	echo $(VERSION) >$(distdir)/build-aux/tarball-version
+
+install-recursive install-exec-recursive install-data-recursive installcheck-recursive: check-version
+
+VERSION_GEN = (cd $(top_srcdir) \
+	       && build-aux/git-version-gen build-aux/tarball-version s/$(PACKAGE_NAME)-/v/ $(PACKAGE_NAME))
+
+.PHONY: check-version _version
+check-version:
+	set -e; \
+	if ! test "x$(VERSION)" = "x`$(VERSION_GEN)`"; then \
+	   echo "Version string not up to date: run 'make _version' first." >&2; \
+	   exit 1; \
+	fi
+
+_version:
+	cd $(srcdir) && rm -rf autom4te.cache .version && $${AUTORECONF:-autoreconf}
+	$(MAKE) .version
