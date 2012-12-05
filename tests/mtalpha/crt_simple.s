775a71e598c06a08b9037987bcfc3f2e1cedf78c
Raphael 'kena' Poss <r.poss@uva.nl>
2012-12-05 11:39:26 +0100

    Fix and simplify the slrt for "common" tests.
    The slrt code (entry point for SL programs compiled from source code)
    contained instructions to preset registers to zero and to print a
    message after the test program terminates. The former is not any more
    necessary since local registers now automatically read zero. The
    latter is not necessary since MGSim now provides a facility for
    programs to request termination with a status code.

--- tests/mtalpha/crt_simple.s
+++ tests/mtalpha/crt_simple.s
@@ -21,72 +21,13 @@ _start:
 	ldfp $l18
 	mov $l18, $l16 # set up frame pointer
         
-	clr $l8 # flush callee-save reg
-	clr $l9 # flush callee-save reg
-	clr $l10 # flush callee-save reg
-	clr $l11 # flush callee-save reg
-	clr $l12 # flush callee-save reg
-	clr $l13 # flush callee-save reg
-	fclr $lf11 # flush callee-save reg
-	fclr $lf12 # flush callee-save reg
-	fclr $lf13 # flush callee-save reg
-	fclr $lf14 # flush callee-save reg
-	fclr $lf15 # flush callee-save reg
-	fclr $lf16 # flush callee-save reg
-	fclr $lf17 # flush callee-save reg
-	fclr $lf18 # flush callee-save reg
-	fclr $lf3 # init FP return reg
-        clr $l2 # flush arg reg
-	clr $l3 # flush arg reg
-        clr $l4 # flush arg reg
-        clr $l5 # flush arg reg
-        clr $l6 # flush arg reg
-        clr $l7 # flush arg reg
-	fclr $lf5 # flush arg reg
-	fclr $lf6 # flush arg reg
-	fclr $lf7 # flush arg reg
-	fclr $lf8 # flush arg reg
-	fclr $lf9 # flush arg reg
-	fclr $lf10 # flush arg reg
-        
 	# call test(void)
 	ldq $l14,test($l17) !literal!2
 	jsr $l15,($l14),test !lituse_jsr!2
         swch
 	ldgp $l17,0($l15)
 
-	bne $l1, $bad
-        swch
-        nop
+        stq $l1, 0x278($31)
 	end
-$bad:
-	ldah $l3, $msg($l17) !gprelhigh
-	lda $l2, 115($l31)  # char <- 's'
-	lda $l3, $msg($l3) !gprellow
-	.align 4
-$L0:
-	print $l2, 194  # print char -> stderr
-	lda $l3, 1($l3)
-	ldbu $l2, 0($l3)
-	sextb $l2, $l2
-        swch
-	bne $l2, $L0
-        swch
-
-	print $l1, 130 # print int -> stderr
-        lda $l1, 10($l31) # NL
-	print $l1, 194  # print char -> stderr
-$fini:	
-	nop
-	nop
-	pal1d 0
-	br $fini
-        swch
 	.end _start
 
-	.section .rodata
-$msg:	
-	.ascii "slrt: main returned \0"
-
-
-        
