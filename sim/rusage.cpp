9837714f4916a5a9ca98479d534eac11e0598808
Raphael 'kena' Poss <r.poss@uva.nl>
2013-07-25 17:29:31 +0200

    Provide memory statistics on Linux using /proc/self/statm if getrusage is broken.

--- sim/rusage.cpp
+++ sim/rusage.cpp
@@ -1,5 +1,7 @@
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <cstdio>
 #include <sys_config.h>
 #include "rusage.h"
@@ -22,8 +24,21 @@ ResourceUsage::ResourceUsage(bool sample, bool set_initial)
 
 	m_utime = res.ru_utime.tv_sec * 1000000ll + res.ru_utime.tv_usec;
 	m_stime = res.ru_stime.tv_sec * 1000000ll + res.ru_stime.tv_usec;
-#ifdef HAVE_STRUCT_RUSAGE_RU_MAXRSS
+#if defined(HAVE_STRUCT_RUSAGE_RU_MAXRSS) && !defined(RUSAGE_MAX_RSS_EMPTY)
 	m_maxrss = res.ru_maxrss / RUSAGE_MAX_RSS_DIVIDER;
+#elif defined(HAVE_PROC_STATM)
+	int fd;
+	char buf[64] = {0};
+	if ((fd = open("/proc/self/statm", O_RDONLY)) != -1)
+	{
+            if (read(fd, buf, 63) != -1)
+            {
+		long v = 0;
+		sscanf(buf, "%*d %ld", &v);
+		m_maxrss = v;
+            }
+            close(fd);
+	}
 #endif
 
 	if (!set_initial)
