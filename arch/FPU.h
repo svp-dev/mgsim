e91c53149a5bf6e80598dd3b538472dd874f9cb5
Raphael 'kena' Poss <r.poss@uva.nl>
2013-07-29 10:37:33 +0200

    Cleanup whitespace.

--- arch/FPU.h
+++ arch/FPU.h
@@ -37,64 +37,64 @@ class FPU : public Object, public Inspect::Interface<Inspect::Read>
     /// Represents an FP operation
     struct Operation
     {
-	    FPUOperation op;
-	    int          size;
-	    double       Rav, Rbv;
-	    RegAddr      Rc;
+            FPUOperation op;
+            int          size;
+            double       Rav, Rbv;
+            RegAddr      Rc;
             std::string  str() const;
     };
 
     /// Represents a source for this FPU
-        class Source : public Object
-	{
-        private:
-	    Buffer<Operation>        inputs;     ///< Input queue for operations from this source
-            StorageTraceSet          outputs;    ///< Set of storage trace each output can generate
-	    DRISC::RegisterFile* regfile;    ///< Register file to write back results for this source
-	    CycleNo                  last_write; ///< Last time an FPU pipe wrote back to this source
-	    unsigned int             last_unit;  ///< Unit that did the last (or current) write
+    class Source : public Object
+    {
+    private:
+        Buffer<Operation>        inputs;     ///< Input queue for operations from this source
+        StorageTraceSet          outputs;    ///< Set of storage trace each output can generate
+        DRISC::RegisterFile* regfile;    ///< Register file to write back results for this source
+        CycleNo                  last_write; ///< Last time an FPU pipe wrote back to this source
+        unsigned int             last_unit;  ///< Unit that did the last (or current) write
 
-            friend class FPU;
-        public:
-            Source(const std::string& name, Object& parent, Clock& clock, Config& config);
-            Source(const Source&) = delete;
-            Source& operator=(const Source&) = delete;
-	};
+        friend class FPU;
+    public:
+        Source(const std::string& name, Object& parent, Clock& clock, Config& config);
+        Source(const Source&) = delete;
+        Source& operator=(const Source&) = delete;
+    };
 
     /// Represents the result of an FP operation
-	struct Result
-	{
-		RegAddr       address;     ///< Address of destination register of result.
-		MultiFloat    value;       ///< Resulting value of the operation.
-		unsigned int  source;      ///< The source of the operation
-		unsigned int  size;        ///< Size of the resulting value.
-		unsigned int  state;       ///< Progression through the pipeline.
-		unsigned int  index;       ///< Current index of writeback.
-	};
+    struct Result
+    {
+            RegAddr       address;     ///< Address of destination register of result.
+            MultiFloat    value;       ///< Resulting value of the operation.
+            unsigned int  source;      ///< The source of the operation
+            unsigned int  size;        ///< Size of the resulting value.
+            unsigned int  state;       ///< Progression through the pipeline.
+            unsigned int  index;       ///< Current index of writeback.
+    };
 
     /// Represents a pipeline for an FP operation type
-	struct Unit
-	{
-	    CycleNo            latency;     ///< The latency of the unit/pipeline
-	    std::deque<Result> slots;       ///< The pipeline slots
-	    bool               pipelined;   ///< Is it a pipeline or a single ex. unit?
+    struct Unit
+    {
+        CycleNo            latency;     ///< The latency of the unit/pipeline
+        std::deque<Result> slots;       ///< The pipeline slots
+        bool               pipelined;   ///< Is it a pipeline or a single ex. unit?
 
         Unit() : latency(0), slots(), pipelined(false) {}
-	};
+    };
 
     /**
      * Called when an operation has completed.
      * @param res the result to write back to the register file.
      * @return true if the result could be written back to the register file.
      */
-	bool OnCompletion(unsigned int unit, const Result& res) const;
+    bool OnCompletion(unsigned int unit, const Result& res) const;
 
-	/**
-	 * Called in order to compute the result from a queued operation
-	 * @param op    [in] the operation with source information
-	 * @param start [in] the cycle number when the FP operation started
-	 * @return the result of the source operation
-	 */
+    /**
+     * Called in order to compute the result from a queued operation
+     * @param op    [in] the operation with source information
+     * @param start [in] the cycle number when the FP operation started
+     * @return the result of the source operation
+     */
     Result CalculateResult(const Operation& op) const;
 
     StorageTraceSet CreateStoragePermutation(size_t num_sources, std::vector<bool>& visited);
@@ -121,13 +121,13 @@ public:
     /// Destroys the FPU object
     ~FPU();
 
-	/**
-	 * @brief Registers a source to the FPU
-	 * @param regfile [in] the register file to use to write back results for this source
-	 * @param output [in] the storage traces that can be generated when writing the result
-	 * @return the unique for this source to be passed to QueueOperation
-	 */
-	size_t RegisterSource(DRISC::RegisterFile& regfile, const StorageTraceSet& output);
+    /**
+     * @brief Registers a source to the FPU
+     * @param regfile [in] the register file to use to write back results for this source
+     * @param output [in] the storage traces that can be generated when writing the result
+     * @return the unique for this source to be passed to QueueOperation
+     */
+    size_t RegisterSource(DRISC::RegisterFile& regfile, const StorageTraceSet& output);
 
     /**
      * @brief Queues an FP operation.
@@ -141,12 +141,12 @@ public:
      * @param Rc      address of the destination register(s)
      * @return true if the operation could be queued.
      */
-	bool QueueOperation(size_t source, FPUOperation op, int size, double Rav, double Rbv, const RegAddr& Rc);
+    bool QueueOperation(size_t source, FPUOperation op, int size, double Rav, double Rbv, const RegAddr& Rc);
 
-	StorageTraceSet GetSourceTrace(size_t source) const;
+    StorageTraceSet GetSourceTrace(size_t source) const;
 
-	// Processes
-	Process p_Pipeline;
+    // Processes
+    Process p_Pipeline;
 
     void Cmd_Info(std::ostream& out, const std::vector<std::string>& arguments) const;
     void Cmd_Read(std::ostream& out, const std::vector<std::string>& arguments) const;
@@ -154,4 +154,3 @@ public:
 
 }
 #endif
-
