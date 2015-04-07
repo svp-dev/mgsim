b44c87941f1c6859e069bd233f24333932b49fdd
Raphael 'kena' Poss <knz@thaumogen.net>
2015-04-07 09:57:44 +0200

    Extend the io tests to check for long I/O operations.

--- tests/common/io.c
+++ tests/common/io.c
@@ -11,7 +11,7 @@
 
 #include "mtconf.h"
 
-char buf[500] = { 0 };
+char buf[4321] = { 0 };
 
 __attribute__((always_inline))
 void die(const char *where)
@@ -25,39 +25,73 @@ void die(const char *where)
 
 const char *testconf = "\0PLACES: 1"; // for "make check": this program is single-threaded.
 
+__attribute__((always_inline))
+void fill(void)
+{
+    int i;
+    strcpy(buf, "hello");
+    for (i = 6; i < sizeof(buf) - 5; ++i)
+        buf[i] = (char)(i & 0xff);
+    strcpy(buf + sizeof(buf) - 6, "hello");
+}
+
 int test(void)
 {
     sys_detect_devs();
     sys_conf_init();
-    
+
     int fd;
     int i;
-    
+
     if ((fd = open("testinput", O_WRONLY|O_TRUNC|O_CREAT, 0666)) == -1)
         die("open1");
-    
+
+
     for (i = 0; i < 100; ++i)
-      if (write(fd, "hello", 5) != 5)
-       die("write");
-        
-   if (close(fd) != 0)
+        if (write(fd, "hello", 5) != 5)
+            die("write");
+
+    if (close(fd) != 0)
         die("close1");
 
     if ((fd = open("testinput", O_RDONLY)) == -1)
-       die("open2");
-    
+        die("open2");
+
     char *p = buf;
     for (i = 0; i < 100; ++i)
-   {
-      if (read(fd, p, 5) != 5)
-        die("read");
-      if (strncmp(p, "hello", 5) != 0)
-        die("data mismatch");
-      p += 5;
-   }
-        
-   if (close(fd) != 0)
-      die("close2");
+    {
+        memset(p, 0, 5);
+        if (read(fd, p, 5) != 5)
+            die("read");
+        if (strncmp(p, "hello", 5) != 0)
+            die("data mismatch");
+        p += 5;
+    }
+
+    if (close(fd) != 0)
+        die("close2");
+
+    if ((fd = open("testinput", O_WRONLY|O_TRUNC, 0666)) == -1)
+        die("open3");
+    fill();
+    if (write(fd, buf, sizeof(buf)) != sizeof(buf))
+        die("write2");
+    if (close(fd) != 0)
+        die("close3");
+
+    if ((fd = open("testinput", O_RDONLY)) == -1)
+        die("open4");
+    memset(buf, 0, sizeof(buf));
+    if (read(fd, buf, sizeof(buf)) != sizeof(buf))
+        die("read2");
+
+    if (strncmp(buf, "hello", 5) != 0)
+        die("data mismatch2");
+    if (strncmp(buf + sizeof(buf) - 6, "hello", 5) != 0)
+        die("data mismatch3");
+    if (close(fd) != 0)
+        die("close4");
+
 
     return 0;
 }
